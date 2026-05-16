23dd7a1007d6142e92693a49461f36abe8c444d5
Ruvim <ruvimsavitsky@gmail.com>
2026-05-16 23:21:43 +0200

    Migrate app init to registry and add net_init.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,39 +3,25 @@
 #include "../kernel/vga.h"
 #include "../kernel/keyboard.h"
 #include "../kernel/timer.h"
-#include "../kernel/gui.h"
 #include "../kernel/fs.h"
+#include "../kernel/net.h"
+#include "../kernel/irq.h"
 #include "../kernel/string.h"
 #include "../kernel/sv_desktop.h"
-#include "../apps/apps.h"
-
-static void open_about(void) {
-}
 
 void kernel_main(void) {
-
-#if SAVAOS_GFX_DEMO
-    savaos_gfx_irq_demo();
-#endif
+    
 
     vga_init();
     kb_init();
     timer_init();
-
-    gui_init();
-
+    irq_system_init();
     fs_init();
+    (void)net_init();
 
-    app_terminal_init();
-    app_notepad_init();
-    app_calc_init();
-    app_filemanager_init();
-
-    gui_add_icon(1, "Terminal",  '\x10', app_terminal_open);
-    gui_add_icon(1, "Notepad",   '\xFE', app_notepad_open);
-    gui_add_icon(1, "Calc",      '\xF0', app_calc_open);
-    gui_add_icon(1, "Files",     '\x1A', app_filemanager_open);
-    gui_add_icon(1, "About",     '\x01', open_about);
-
-    gui_main_loop();
+#if SAVAOS_GFX_DEMO
+    savaos_gfx_irq_demo();
+#else
+    savaos_desktop_run();
+#endif
 }
